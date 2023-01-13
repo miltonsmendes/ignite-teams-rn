9f14b2b59952afdd04b819de7c9ba9e5c8382149
unknown <miltonsergiom@gmail.com>
2023-01-13 01:11:56 -0300

    Added Groups, Header, GroupsCard, Hightlight, Loading and Button components

---
+++ src/components/Loading/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components/native";
+
+export const Container = styled.SafeAreaView`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+
+  background-color: ${({ theme }) => theme.COLORS.GRAY_600};
+`;
+
+export const LoadIndicator = styled.ActivityIndicator.attrs(({ theme }) => ({
+  color: theme.COLORS.GREEN_700,
+}))``;
